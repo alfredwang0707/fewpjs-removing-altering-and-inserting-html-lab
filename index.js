c6aa76b21042da4f28640e7aa0bd7bdd40de93d3
alfredwang0707 <alfredwang0707@gmail.com>
2020-12-09 16:27:49 -0500

    commit tree 078adc46a393ebe18406e1a79ddcc9761c17891e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+// document.querySelector("main#main").remove
+// will not work w/o ()
+
+//expect(newHeader.nodeName 
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Alfred-Wang is the champion"
